26b46962d479f8a780c676bf4be6ef1f14c9ee66
thisisnadya <nadyamumtazah33@gmail.com>
2022-06-01 20:21:58 +0700

    add homenav components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
+import HomeNav from "./components/HomeNav";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <HomeNav />
         <Search />
         <Category />
         <Pages />
---
+++ src/components/HomeNav.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiPopcorn } from "react-icons/gi";
+
+function HomeNav() {
+  return (
+    <Nav>
+      <Link to={"/"}>
+        <h3>showtime</h3>
+      </Link>
+      <GiPopcorn />
+    </Nav>
+  );
+}
+
+const Nav = styled.div`
+  width: 50%;
+  margin: 0 auto;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  svg {
+    font-size: 2rem;
+    color: white;
+  }
+`;
+
+export default HomeNav;
