a0a9b0e5f6c7755885d52aeff5e6c99192a6d503
thisisnadya <nadyamumtazah33@gmail.com>
2022-06-01 18:54:13 +0700

    show searched

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -15,10 +15,45 @@ function Searched() {
       `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${title}`
     );
     const data = await api.json();
+    setSearched(data.results);
     console.log(data.results);
   };
 
-  return <div>Searched</div>;
+  return (
+    <Grid className="mt-5">
+      {searched.map((item) => {
+        return (
+          <Card key={item.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+              alt=""
+            />
+          </Card>
+        );
+      })}
+    </Grid>
+  );
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(12rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    border-radius: 1rem;
+    width: 100%;
+    height: 100%;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Searched;
