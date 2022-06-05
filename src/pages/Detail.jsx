f13c84974b8dcc7398f9641c709194b49b1f9234
thisisnadya <nadyamumtazah33@gmail.com>
2022-06-05 20:53:06 +0700

    edit detail poster size

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -17,9 +17,9 @@ function Detail() {
     console.log(data);
   };
   return (
-    <Wrapper className="mt-5">
+    <Wrapper className="mt-5 d-flex">
       <img
-        src={`https://image.tmdb.org/t/p/w500${detail.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w342${detail.poster_path}`}
         alt=""
       />
       <div className="ms-5">
@@ -28,6 +28,7 @@ function Detail() {
         ) : (
           <h1>{detail.original_title}</h1>
         )}
+        <h4 className="rate">{detail.vote_average}</h4>
         <p>{detail.overview}</p>
       </div>
     </Wrapper>
@@ -35,8 +36,20 @@ function Detail() {
 }
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
+  /* display: flex;
+  justify-content: center; */
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  h3 {
+    font-family: sans-serif;
+  }
+
+  p {
+    color: #eeeeee;
+  }
 `;
 
 export default Detail;
--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -39,7 +39,10 @@ function Searched() {
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(12rem, 1fr));
+  /* grid-template-columns: repeat(auto-fit, minmax(12rem, 1fr)); */
+  grid-template-columns: repeat(auto-fit, minmax(12rem, 0.17fr));
+  /* grid-auto-columns: minmax(0, 1fr); */
+  /* grid-auto-flow: column; */
   grid-gap: 3rem;
 `;
 
